3480f5382b5fbeb6146b5325fc5da84ef780ee81
Hannah Ryan <hjryan@uw.edu>
2020-11-05 20:59:14 -0800

    trying flask

---
+++ flaskblog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+	{
+	'author': 'Hot Dog',
+	'title': 'blog post 1',
+	'content': 'akjkgjskjgtkwtg',
+	'date_posted': 'April 20, 2018'
+	},
+	{
+	'author': 'Hit Dig',
+	'title': 'blog post 3000',
+	'content': 'dflkfa',
+	'date_posted': 'December 20, 2018'
+	}
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+	app.run(debug=True)
