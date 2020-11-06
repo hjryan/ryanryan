4964eb729927ead797def2d38906a37c6617ccd5
Hannah Ryan <hjryan@uw.edu>
2020-11-06 15:15:39 -0800

    add back secret key lol

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'gilq34uiufgo39qwo7867854ww'
 
 user = {
     'firstName': "First Name"
@@ -45,6 +46,6 @@ def login():
         else:
             flash('Login unsuccessful! Please try again.', 'danger')
     return render_template('login.html', title='Log In', form=form)
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
