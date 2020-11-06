f9503053964494f9b551c95cef2657fa9064c555
Hannah Ryan <hjryan@uw.edu>
2020-11-06 14:30:33 -0800

    uhhh

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.firstName.data}!', 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('index'), name=form.firstName.data)
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
