f82a1f62893648aa048a5cd284671f298915d024
Hannah <hjryan@users.noreply.github.com>
2020-11-21 14:10:39 -0800

    Update setup-queries.sql

--- setup-queries.sql
+++ setup-queries.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS Activities;
 DROP TABLE IF EXISTS ActivitiesUsers;
 DROP TABLE IF EXISTS ActivitiesLocales;
 CREATE TABLE Users(
-userID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+userID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
 firstName VARCHAR(40) NOT NULL,
 lastName VARCHAR(40) NOT NULL,
 localeID INT
@@ -13,7 +13,7 @@ localeID INT
 INSERT INTO Users(firstName, lastName) VALUES("Hannah", "Ryan");
 INSERT INTO Users(firstName, lastName) VALUES("Ryan", "McKenzie");
 CREATE TABLE Locales(
-localeID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+localeID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
 localeName VARCHAR(40) NOT NULL
 );
 INSERT INTO Locales(localeName) VALUES("Seattle");
@@ -29,7 +29,7 @@ UPDATE Users
 SET localeID = 2
 WHERE userID = 2;
 CREATE TABLE Walks(
-walkID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+walkID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
 walkName VARCHAR(40) NOT NULL,
 destination INT,
 origin INT,
@@ -41,7 +41,7 @@ FOREIGN KEY(userID) REFERENCES Users(userID) ON DELETE SET NULL
 INSERT INTO Walks(walkName, destination, origin, userID) VALUES("Very long walk", "2", "1", "1");
 INSERT INTO Walks(walkName, destination, origin, userID) VALUES("Very long walk in reverse", "1", "2", "2");
 CREATE TABLE Activities(
-activityID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+activityID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
 activityName VARCHAR(40) NOT NULL
 );
 INSERT INTO Activities(activityName) VALUES("Call Arvind I guess");
