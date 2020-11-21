705a95b34861a43231550dd2e83334f827e09e4e
Hannah <hjryan@users.noreply.github.com>
2020-11-21 14:14:57 -0800

    Update setup-queries.sql

--- setup-queries.sql
+++ setup-queries.sql
@@ -5,15 +5,15 @@ DROP TABLE IF EXISTS Activities;
 DROP TABLE IF EXISTS ActivitiesUsers;
 DROP TABLE IF EXISTS ActivitiesLocales;
 CREATE TABLE Users(
-userID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+userID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 firstName VARCHAR(40) NOT NULL,
 lastName VARCHAR(40) NOT NULL,
-localeID INT
+localeID INTEGER
 );
 INSERT INTO Users(firstName, lastName) VALUES("Hannah", "Ryan");
 INSERT INTO Users(firstName, lastName) VALUES("Ryan", "McKenzie");
 CREATE TABLE Locales(
-localeID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+localeID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 localeName VARCHAR(40) NOT NULL
 );
 INSERT INTO Locales(localeName) VALUES("Seattle");
@@ -29,11 +29,11 @@ UPDATE Users
 SET localeID = 2
 WHERE userID = 2;
 CREATE TABLE Walks(
-walkID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+walkID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 walkName VARCHAR(40) NOT NULL,
-destination INT,
-origin INT,
-userID INT,
+destination INTEGER,
+origin INTEGER,
+userID INTEGER,
 FOREIGN KEY(destination) REFERENCES Locales(localeID) ON DELETE SET NULL,
 FOREIGN KEY(origin) REFERENCES Locales(localeID) ON DELETE SET NULL,
 FOREIGN KEY(userID) REFERENCES Users(userID) ON DELETE SET NULL
@@ -41,22 +41,22 @@ FOREIGN KEY(userID) REFERENCES Users(userID) ON DELETE SET NULL
 INSERT INTO Walks(walkName, destination, origin, userID) VALUES("Very long walk", "2", "1", "1");
 INSERT INTO Walks(walkName, destination, origin, userID) VALUES("Very long walk in reverse", "1", "2", "2");
 CREATE TABLE Activities(
-activityID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+activityID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 activityName VARCHAR(40) NOT NULL
 );
 INSERT INTO Activities(activityName) VALUES("Call Arvind I guess");
 INSERT INTO Activities(activityName) VALUES("Enjoy operating the flip");
 CREATE TABLE ActivitiesUsers(
-activityID INT,
-userID INT,
+activityID INTEGER,
+userID INTEGER,
 FOREIGN KEY(activityID) REFERENCES Activities(activityID) ON DELETE SET NULL,
 FOREIGN KEY(userID) REFERENCES Users(userID) ON DELETE SET NULL
 );
 INSERT INTO ActivitiesUsers(activityID, userID) VALUES (1,1);
 INSERT INTO ActivitiesUsers(activityID, userID) VALUES (2,2);
 CREATE TABLE ActivitiesLocales(
-activityID INT,
-localeID INT,
+activityID INTEGER,
+localeID INTEGER,
 FOREIGN KEY(activityID) REFERENCES Activities(activityID) ON DELETE SET NULL,
 FOREIGN KEY(localeID) REFERENCES Locales(localeID) ON DELETE SET NULL
 );
