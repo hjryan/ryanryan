664167ddd717c92ef2997f2523954b76e1c33018
Hannah <hjryan@users.noreply.github.com>
2020-11-21 14:27:48 -0800

    Update setup-queries.sql

--- setup-queries.sql
+++ setup-queries.sql
@@ -5,21 +5,18 @@ DROP TABLE IF EXISTS Walks;
 DROP TABLE IF EXISTS Activities;
 DROP TABLE IF EXISTS ActivitiesUsers;
 DROP TABLE IF EXISTS ActivitiesLocales;
-CREATE TABLE Users(
-userID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-firstName VARCHAR(40) NOT NULL,
-lastName VARCHAR(40) NOT NULL,
-localeID INTEGER
-);
-INSERT INTO Users(firstName, lastName) VALUES("Hannah", "Ryan");
-INSERT INTO Users(firstName, lastName) VALUES("Ryan", "McKenzie");
 CREATE TABLE Locales(
 localeID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 localeName VARCHAR(40) NOT NULL
 );
 INSERT INTO Locales(localeName) VALUES("Seattle");
 INSERT INTO Locales(localeName) VALUES("Denver");
-ALTER TABLE Users
-ADD FOREIGN KEY(localeID)
-REFERENCES Locales(localeID)
-ON DELETE SET NULL;
+CREATE TABLE Users(
+userID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+firstName VARCHAR(40) NOT NULL,
+lastName VARCHAR(40) NOT NULL,
+localeID INTEGER,
+FOREIGN KEY(localeID) REFERENCES Locales(localeID) ON DELETE SET NULL
+);
+INSERT INTO Users(firstName, lastName, localeID) VALUES("Hannah", "Ryan", "1");
+INSERT INTO Users(firstName, lastName, localeID) VALUES("Ryan", "McKenzie", "2");
