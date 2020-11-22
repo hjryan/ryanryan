7ee4523980928ec2746a147ab2da7089553fa2d5
Hannah Ryan <hjryan@uw.edu>
2020-11-21 20:56:41 -0800

    nope

--- app.py
+++ app.py
@@ -123,10 +123,9 @@ def register():
     # if so, is someone there? don't let them register there
 
     # otherwise, add locale
-    def addLocale(localeName=None):  
-        if localeName:
-            added = cur.execute("INSERT INTO Locales (localeName) VALUES (?)", (localeName,))
-            db.commit()
+    added = cur.execute("INSERT INTO Locales (localeName) VALUES (?)", (localeName,))
+    db.commit()
+    db.close()
     
     # localeID = cur.execute("SELECT localeID FROM Locales WHERE localeName = (?)", [localeName]).fetchall()
 
@@ -134,7 +133,7 @@ def register():
         flash(f"{form.firstName.data}'s account created!", 'success')
         # added = cur.execute("INSERT INTO Users (firstName, lastName, localeID) VALUES (?, ?, ?)", (firstName, lastName, localeID,))
         # db.commit()
-        db.close()
+        
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
