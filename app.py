c53a42c1b5da2c7d0a13c388cceec8e5345941bb
Hannah Ryan <hjryan@uw.edu>
2020-11-21 20:54:07 -0800

    try to just add locale

--- app.py
+++ app.py
@@ -118,8 +118,6 @@ def register():
     firstName = {form.firstName.data}
     lastName = {form.lastName.data}
     localeName = {form.localeName.data}
-    localeID = cur.execute("SELECT localeID FROM Locales WHERE localeName = (?)", [localeName]).fetchall()
-    db.commit()
     # additional validations needed --
     # use locales data to check if locale already exists
     # if so, is someone there? don't let them register there
@@ -130,10 +128,12 @@ def register():
             added = cur.execute("INSERT INTO Locales (localeName) VALUES (?)", (localeName,))
             db.commit()
     
+    # localeID = cur.execute("SELECT localeID FROM Locales WHERE localeName = (?)", [localeName]).fetchall()
+
     if form.validate_on_submit():
         flash(f"{form.firstName.data}'s account created!", 'success')
-        added = cur.execute("INSERT INTO Users (firstName, lastName, localeID) VALUES (?, ?, ?)", (firstName, lastName, localeID,))
-        db.commit()
+        # added = cur.execute("INSERT INTO Users (firstName, lastName, localeID) VALUES (?, ?, ?)", (firstName, lastName, localeID,))
+        # db.commit()
         db.close()
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
