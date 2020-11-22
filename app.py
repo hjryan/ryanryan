e729f2e035aaa4d49e9f9a43556ea894a77104fe
Hannah Ryan <hjryan@uw.edu>
2020-11-21 21:56:20 -0800

    remove print

--- app.py
+++ app.py
@@ -120,7 +120,6 @@ def completeRegistration(localeName=None):
     if localeName:
         localeAdded = cur.execute("INSERT INTO Locales (localeName) VALUES (?)", (localeName,))
         localeID = (cur.execute("SELECT localeID FROM Locales WHERE localeName = (?)", [localeName]).fetchone())[0]
-        print(localeID[0])
         userAdded = cur.execute("INSERT INTO Users (firstName, lastName, localeID) VALUES (?, ?, ?)", (firstName, lastName, localeID,))
         #user.userID = cur.execute("SELECT userID from Users WHERE firstName = (?) AND lastName = (?) AND localeID = (?)", (firstName, lastName, localeID,)).fetchall()
     db.commit()
