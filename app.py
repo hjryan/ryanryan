275fb01cc85c720df58c46cd8c894cbdb5e4a54d
Hannah Ryan <hjryan@uw.edu>
2020-11-21 21:09:44 -0800

    again

--- app.py
+++ app.py
@@ -110,32 +110,24 @@ def locales():
     db.close()
     return render_template('locales.html', title='Locales', data=data)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register(localeName=None):
-    form = RegistrationForm()
+@app.route('/complete-registration', methods=['GET'])
+def completeRegistration(localeName=None):
     db = get_db()
     cur = db.cursor()
-    firstName = {form.firstName.data}
-    lastName = {form.lastName.data}
-    localeName = {form.localeName.data}
-    print(firstName, lastName, localeName)
-    # additional validations needed --
-    # use locales data to check if locale already exists
-    # if so, is someone there? don't let them register there
-
-    # otherwise, add locale
+    localeName = request.args.get('localeName')   
     if localeName:
         added = cur.execute("INSERT INTO Locales (localeName) VALUES (?)", (localeName,))
+        # localeID = cur.execute("SELECT localeID FROM Locales WHERE localeName = (?)", [localeName]).fetchall()
+        # added = cur.execute("INSERT INTO Users (firstName, lastName, localeID) VALUES (?, ?, ?)", (firstName, lastName, localeID,))
     db.commit()
     db.close()
-    
-    # localeID = cur.execute("SELECT localeID FROM Locales WHERE localeName = (?)", [localeName]).fetchall()
+    return redirect('/locales')
 
+@app.route('/register', methods=['GET'])
+def register(localeName=None):
+    form = RegistrationForm()
     if form.validate_on_submit():
         flash(f"{form.firstName.data}'s account created!", 'success')
-        # added = cur.execute("INSERT INTO Users (firstName, lastName, localeID) VALUES (?, ?, ?)", (firstName, lastName, localeID,))
-        # db.commit()
-        
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
